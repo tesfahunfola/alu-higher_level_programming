1940f3a1603c41e4c1529a2da0bbc7897a8ed0db
tesfahunfola <t.zanafola@alustudent.com>
2023-03-24 11:49:46 -0700

    update

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A program to list attributes and methods of an object"""
+
+
+def lookup(obj):
+    """ A function that list attributes and methods of an object"""
+    return dir(obj)
