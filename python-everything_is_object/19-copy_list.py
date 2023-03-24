a5b26af2687fe64b31e11f4994efce8c88cfa3be
tesfahunfola <t.zanafola@alustudent.com>
2023-03-24 05:23:55 -0700

    update

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def copy_list(l):
     return l[:]
+my_list = [1, 2, 3]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
 
