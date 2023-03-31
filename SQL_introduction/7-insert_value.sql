15dabe1ffb2c1b78554e6d44b3dd067a444557f9
tesfahunfola <t.zanafola@alustudent.com>
2023-03-31 01:53:33 -0700

    update

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+--  script that inserts a new row
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- script that displays records
+SELECT COUNT(*) FROM `first_table` WHERE `id` = 89;
