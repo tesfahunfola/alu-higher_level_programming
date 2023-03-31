d0289169d40ee55c81c30b4bfbef76f80e4aaaf3
tesfahunfola <t.zanafola@alustudent.com>
2023-03-31 02:20:28 -0700

    update

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-WHERE month = 7 OR month = 8
-GROUP BY city
-ORDER BY avg_temp DESC
+-- temperatures between July and August.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
