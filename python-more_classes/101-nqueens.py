eaa6da45b259d418292b58a0a506c0cb2107b70c
tesfahunfola <t.zanafola@alustudent.com>
2023-04-04 14:40:58 -0700

    update

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,63 +1,44 @@
 #!/usr/bin/python3
-'''This code is a python program that solves the N queens chess puzzle'''
+import sys
 
-global N
-N = 4
- 
-def printSolution(board):
-    for i in range(N):
-        for j in range(N):
-            print (board[i][j],end=' ')
-        print()
 
-# A utility function to check if a queen can
-# be placed on board[row][col]. Note that this
-# function is called when "col" queens are
-# already placed in columns from 0 to col -1.
-# So we need to check only left side for
-# attacking queens
-def isSafe(board, row, col):
- 
-    # Check this row on left side
-    for i in range(col):
-        if board[row][i] == 1:
+def is_valid(board, row, col):
+    for r, c in board:
+        if c == col or r + c == row + col or r - c == row - col:
             return False
- 
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-     # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
- 
     return True
- 
-def solveNQUtil(board, col):
-    # base case: If all queens are placed
-    # then return true
-    if col >= N:
-        return True
 
-     # Consider this column and try placing
-    # this queen in all rows one by one
-    for i in range(N):
- 
-        if isSafe(board, i, col):
-            # Place this queen in board[i][col]
-            board[i][col] = 1
- 
-            # recur to place rest of the queens
-            if solveNQUtil(board, col + 1) == True:
-                return True
- 
-            # If placing queen in board[i][col
-            # doesn't lead to a solution, then
-            # queen from board[i][col]
-            board[i][col] = 0
 
-    # if the queen can not be placed in any row in
-    # this column col  then return false
-    return False
+def nqueens_helper(n, row, board, solutions):
+    if row == n:
+        solutions.append(board.copy())
+    else:
+        for col in range(n):
+            if is_valid(board, row, col):
+                board.append((row, col))
+                nqueens_helper(n, row + 1, board, solutions)
+                board.pop()
+
+
+def nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solutions = []
+    nqueens_helper(n, 0, [], solutions)
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+        nqueens(n)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
