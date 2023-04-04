37009e8c9bc524ed41d8c2598146a917fef52f97
tesfahunfola <t.zanafola@alustudent.com>
2023-04-04 13:38:12 -0700

    update

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-url=$1
-response=$(curl -s -o /dev/null -w "%{http_code}" $url) # Send a GET request and get the response status code
-if [[ $response -eq 200 ]]; then
-  curl -s $url # If the response status code is 200, display the body of the response
-fi
+# display body if 200
+curl -sLfG "$1"
