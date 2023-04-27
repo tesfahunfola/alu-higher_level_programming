d14ddc80c3a4b15916e5200a7c1cffcc65e933a1
tesfahunfola <t.zanafola@alustudent.com>
2023-04-27 00:46:06 -0700

    update

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-Prints out values in the states table of a db where name matches the argument in a safe way
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
 """
-
-
 import sys
 import MySQLdb
 
@@ -11,9 +10,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
-    states = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
 
     for state in states:
         print(state)
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-""" Script that lists all State objects that contain the letter a from the db"""
-
-
+"""
+Lists all State objects that contain the letter a from the database
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                        sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True
-                    )
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-
-    Base.metadata.create_all(engine)
-
     session = Session()
 
-    states = session.query(State).filter(State.name.contains('a')).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
     for state in states:
-        print(f'{state.id}: {state.name}')
-
-    session.close()
+        print("{}: {}".format(state.id, state.name))
