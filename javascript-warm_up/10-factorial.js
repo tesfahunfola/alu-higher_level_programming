89f2b7c6088cc463170f7e3052edb192217dd51a
tesfahunfola <t.zanafola@alustudent.com>
2023-06-14 12:02:07 -0700

    update

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function factorial (n) {
-	if (isNaN(n)) {
-		return 1;
-	}else if 
-		(n == 1 || n == 0){
-			return 1;
+  if (isNaN(n)) {
+    return 1;
+  }else if
+  (n === 1 || n === 0) {
+	return 1;
 		} else {
 			return n * factorial (n-1);
 		}
