f064e289f34d634857041027a94dce148f388a54
tesfahunfola <t.zanafola@alustudent.com>
2023-06-14 08:19:24 -0700

    update

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const languages = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
 for (let i = 0, i < languages.length, i++) {
-  console.log(languages)}
+  console.log(languages[i])}
