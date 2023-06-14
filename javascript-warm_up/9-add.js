e8a277dd7670fe30734a2a78aa13018cc91f74c7
tesfahunfola <t.zanafola@alustudent.com>
2023-06-14 09:35:03 -0700

    update

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add (a, b) {
+	return a + b;
+}
+const num1 =parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)){
+	console.log('Missing Integers');
+}else {
+	const result = add(num1 +num2)
+	console.log(result);
+}
