00f01973184ba1c1b79aac8a59aca25a6634230b
tesfahunfola <t.zanafola@alustudent.com>
2023-06-15 10:40:38 -0700

    update

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-const argument1 = 'Python';
-const argument2 = 'fun';
 
-if (argument1 === undefined || argument2 === undefined) {
-  console.log('HBTN is undefined');
-} else if (argument1 === undefined && argument2 === undefined) {
-  console.log('undefined is undefined');
-} else {
-  console.log(argument1 + ' is ' + argument2);
-}
+const args = process.argv.slice(2);
+
+console.log(args[0] + ' is ' + args[1]);
