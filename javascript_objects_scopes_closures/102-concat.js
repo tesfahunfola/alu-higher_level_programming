4caa5c2204b3073da76f022378c56c60aafc29e2
tesfahunfola <t.zanafola@alustudent.com>
2023-06-16 02:49:48 -0700

    update

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -5,8 +5,8 @@ const sourceFile1 = process.argv[2];
 const sourceFile2 = process.argv[3];
 const destinationFile = process.argv[4];
 
-const content1 = fs.readFileSync(sourceFile1, 'utf8');
-const content2 = fs.readFileSync(sourceFile2, 'utf8');
-const concatenatedContent = content1 + '\n' + content2;
+const content1 = fs.readFileSync(sourceFile1, 'utf8').trim();
+const content2 = fs.readFileSync(sourceFile2, 'utf8').trim();
+const concatenatedContent = content1 + '\n' + content2 + '\n';
 
 fs.writeFileSync(destinationFile, concatenatedContent);
