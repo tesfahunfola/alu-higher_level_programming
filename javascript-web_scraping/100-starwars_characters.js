c99fea5f10545e2165a7680bacace0283ea5fdc5
tesfahunfola <t.zanafola@alustudent.com>
2023-06-16 08:39:29 -0700

    update

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Get the movie ID from the command line argument
+const movieId = process.argv[2];
+
+// Make a GET request to the Star Wars API to fetch the movie details
+request.get(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+    // Iterate over the character URLs and fetch each character's details
+    characters.forEach((characterUrl) => {
+      request.get(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
