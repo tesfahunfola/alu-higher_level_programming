b9449916b4399ff6bb42d84b46b34eace4d336bf
tesfahunfola <t.zanafola@alustudent.com>
2023-06-16 02:25:46 -0700

    update

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {}; // Return an empty object if width or height is not positive
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
