02d681bffb2c9e2474ef4afc15e49b85b4e03e01
tesfahunfola <t.zanafola@alustudent.com>
2023-06-16 11:14:18 -0700

    update

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,43 +1,24 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-// Get the movie ID from the command line argument
-const movieId = process.argv[2];
+function helpRequest (arr, i) {
+  if (i === arr.length) {
+    return;
+  }
+  request(arr[i], function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(JSON.parse(body).name);
+    helpRequest(arr, i + 1);
+  });
+}
 
-// Make a GET request to the Star Wars API to fetch the movie details
-request.get(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
-  } else {
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    // Create an array to store the character names
-    const characterNames = [];
-
-    // Function to fetch a character's details
-    const fetchCharacter = (url) => {
-      request.get(url, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const character = JSON.parse(body);
-          characterNames.push(character.name);
-
-          // Check if all characters have been fetched
-          if (characterNames.length === characterUrls.length) {
-            // Print the character names in the same order as the characterUrls list
-            characterUrls.forEach((url) => {
-              const characterId = url.split('/').slice(-2, -1)[0];
-              const characterName = characterNames[characterId - 1];
-              console.log(characterName);
-            });
-          }
-        }
-      });
-    };
-
-    // Fetch details for each character in the movie
-    characterUrls.forEach((url) => fetchCharacter(url));
   }
+  const charac = JSON.parse(body).characters;
+  helpRequest(charac, 0);
 });
