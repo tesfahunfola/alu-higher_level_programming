52ae2c6948641571b91475e71ae1efcdcec83f61
tesfahunfola <t.zanafola@alustudent.com>
2023-06-16 02:18:25 -0700

    update

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If either width or height is less than or equal to 0,
-      // create an empty object by not initializing any properties
-      return {};
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
-}
-
-module.exports = Rectangle;
+};
